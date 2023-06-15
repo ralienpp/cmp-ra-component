b67b9498da06138438bd8cb7200043fc3b27fce0
Andreas Kretschmer <andreas.kretschmer@siemens.com>
2023-06-15 13:38:19 +0200

    bugfixes

--- src/main/java/com/siemens/pki/cmpracomponent/cmpextension/KemCiphertextInfo.java
+++ src/main/java/com/siemens/pki/cmpracomponent/cmpextension/KemCiphertextInfo.java
@@ -25,76 +25,62 @@ import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.asn1.DERSequence;
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
 
-	/**
-	 *    KemCiphertextInfo ::= SEQUENCE {
+/**
+ *    KemCiphertextInfo ::= SEQUENCE {
+ *      kem              AlgorithmIdentifier{KEM-ALGORITHM, {...}},
+ *      ct               OCTET STRING
+ *    }
+ */
+public class KemCiphertextInfo extends ASN1Object {
+    private final AlgorithmIdentifier kem;
+    private final ASN1OctetString ct;
+
+    private KemCiphertextInfo(ASN1Sequence seq) {
+        kem = AlgorithmIdentifier.getInstance(seq.getObjectAt(0));
+        ct = ASN1OctetString.getInstance(seq.getObjectAt(1));
+    }
+
+    public KemCiphertextInfo(AlgorithmIdentifier kem, ASN1OctetString ct) {
+        this.kem = kem;
+        this.ct = ct;
+    }
+
+    public static KemCiphertextInfo getInstance(Object o) {
+        if (o instanceof KemCiphertextInfo) {
+            return (KemCiphertextInfo) o;
+        }
+
+        if (o != null) {
+            return new KemCiphertextInfo(ASN1Sequence.getInstance(o));
+        }
+
+        return null;
+    }
+
+    public AlgorithmIdentifier getKem() {
+        return kem;
+    }
+
+    public ASN1OctetString getCt() {
+        return ct;
+    }
+
+    /**
+     * <pre>
+     *    KemCiphertextInfo ::= SEQUENCE {
      *      kem              AlgorithmIdentifier{KEM-ALGORITHM, {...}},
      *      ct               OCTET STRING
      *    }
-	 */
-	public class KemCiphertextInfo
-	    extends ASN1Object
-	{
-	    private final AlgorithmIdentifier kem;
-	    private final ASN1OctetString ct;
+     * </pre>
+     *
+     * @return a basic ASN.1 object representation.
+     */
+    public ASN1Primitive toASN1Primitive() {
+        ASN1EncodableVector v = new ASN1EncodableVector(2);
 
-	    private KemCiphertextInfo(ASN1Sequence seq)
-	    {
-	    	kem = AlgorithmIdentifier.getInstance(seq.getObjectAt(0));
-	    	ct = ASN1OctetString.getInstance(seq.getObjectAt(1));
-	    }
+        v.add(kem);
+        v.add(ct);
 
-
-	    public KemCiphertextInfo(	    
-	    		AlgorithmIdentifier kem,
-	    		ASN1OctetString ct)
-	    {
-	        this.kem = kem;
-	        this.ct = ct;
-	    }
-
-	    public static KemCiphertextInfo getInstance(Object o)
-	    {
-	        if (o instanceof KemCiphertextInfo)
-	        {
-	            return (KemCiphertextInfo)o;
-	        }
-
-	        if (o != null)
-	        {
-	            return new KemCiphertextInfo(ASN1Sequence.getInstance(o));
-	        }
-
-	        return null;
-	    }
-
-
-		public AlgorithmIdentifier getKem() {
-			return kem;
-		}
-
-
-		public ASN1OctetString getCt() {
-			return ct;
-		}
-
-
-		/**
-	     * <pre>
-	     *    KemCiphertextInfo ::= SEQUENCE {
-         *      kem              AlgorithmIdentifier{KEM-ALGORITHM, {...}},
-         *      ct               OCTET STRING
-         *    }
-	     * </pre>
-	     *
-	     * @return a basic ASN.1 object representation.
-	     */
-	    public ASN1Primitive toASN1Primitive()
-	    {
-	        ASN1EncodableVector v = new ASN1EncodableVector(4);
-
-	        v.add(kem);
-	        v.add(ct);
-
-	        return new DERSequence(v);
-	    }
+        return new DERSequence(v);
+    }
 }
